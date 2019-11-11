c182834344954ac65d4b9cb46f1bd098ab924241
unknown <caiovinius@hotmail.com>
2019-11-10 23:17:21 -0300

    formulario aluno criado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="Content">
+        <Form>
+          <FormGroup>
+            <Label for="nome">Nome</Label>
+            <Input type="text" name="nome" id="nome" placeholder="Coloque aqui seu Nome" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input type="email" name="email" id="email" placeholder="Coloque aqui seu Email" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="semestre">Semestre</Label>
+            <Input type="semestre" name="semestre" id="semestre" placeholder="Coloque aqui seu semestre" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="materias">Selecione suas matérias</Label>
+            <Input type="select" name="materias" id="materias" multiple>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="comentarios">Comentário</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
+      </div>
     </div>
   );
 }
