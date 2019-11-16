67c99e5fbe975a3fdc3502da17c5296cfda2fcc9
Caio <caiovinius@hotmail.com>
2019-11-16 18:37:59 -0300

    atualizados os paineis de aluno e professor

--- src/components/Aluno/index.js
+++ src/components/Aluno/index.js
@@ -16,14 +16,32 @@ const Aluno = () => (
         </FormGroup>
         <FormGroup>
           <Label for="matrícula">Matrícula</Label>
-          <Input type="matrícula" name="matrícula" id="matrícula" placeholder="Coloque aqui sua matrícula" />
+          <Input type="text" name="matrícula" id="matrícula" placeholder="Coloque aqui sua matrícula" />
         </FormGroup>
         <FormGroup>
-          <Label for="semestre">Semestre</Label>
-          <Input type="semestre" name="semestre" id="semestre" placeholder="Coloque aqui seu semestre" />
+          <Label for="semestre">Semestre de ingressso</Label>
+          <Input type="text" name="semestre" id="semestre" placeholder="Coloque aqui seu semestre" />
         </FormGroup>
         <FormGroup>
-          <Label for="materias">Matérias</Label>
+          <Label for="cr">CR</Label>
+          <Input type="text" name="cr" id="cr" placeholder="Coloque aqui seu CR" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="trabalho">Você está estagiando ou trabalhando?</Label>
+          <Input type="select" name="trabalho" id="trabalho">
+            <option>Sim</option>
+            <option>Não</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="trabalho">Você está participando de alguma Iniciação Científica ou extensão?</Label>
+          <Input type="select" name="extensao" id="extensao">
+            <option>Sim</option>
+            <option>Não</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="materias">Monte sua grade do semestre</Label>
           <Input type="select" name="materias" id="materias" multiple>
             <option>1</option>
             <option>2</option>
--- src/components/Professor/index.js
+++ src/components/Professor/index.js
@@ -1,66 +1,199 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { 
     Button, 
     Form, 
     FormGroup, 
+    FormText,
     Label, 
     Input,
     Badge,
     Card, CardBody,
-    CardTitle
+    CardTitle,
+    Modal, ModalHeader, ModalBody, ModalFooter,
+    TabContent, TabPane,
+    Nav, NavItem, NavLink, CardText, Row, Col
 } from 'reactstrap';
+import { FaRegEye } from "react-icons/fa";
+import classnames from 'classnames';
+  
 
-const Professor = () => (
+const Professor = (props) => {
+  const {
+    buttonLabel,
+    className,
+    name,
+    matricula,
+    semestre,
+    cr,
+    trabalho,
+    extensao,
+    materias
+  
+   } = props;
+  
+  const [modal, setModal] = useState(false);
+  
+  const toggle = () => setModal(!modal);
+
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggleTab = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+
+  return (
     <div className="Professor">
-        <Button color="info" outline>Solicitações <Badge color="info">3</Badge></Button>
+        <Button className="solicitacaoButton" color="info" outline>Solicitações <Badge color="info">3</Badge></Button>
         <div>
-        <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
-          <div className="solicitacaoItems">     
-            <CardTitle><b>Caio Vinícius Santos Lopes</b></CardTitle>
-            <Badge href="#" color="warning" pill>Vizualizado</Badge>
-          </div>
-          <CardBody className="simpleCard">
-          </CardBody>
-        </Card>
-        <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
-          <div className="solicitacaoItems">     
-            <CardTitle className="solicitacaoTitle"><b>Daniele Freire Santos</b></CardTitle>
-            <Badge href="#" color="info" pill>Novo</Badge>
-          </div>
-          <CardBody className="simpleCard">
-          </CardBody>
-        </Card>
-        <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
-          <div className="solicitacaoItems">     
-            <CardTitle className="solicitacaoTitle"><b>Daniele Freire Santos</b></CardTitle>
-            <Badge href="#" color="success" pill>Revisado</Badge>
-          </div>
-          <CardBody className="simpleCard">
-          </CardBody>
-        </Card>
-      
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => { toggleTab('1'); }}
+              >
+                <Badge href="#" color="secondary" pill>Todas</Badge>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => { toggleTab('2'); }}
+              >
+                <Badge href="#" color="info" pill>Novos</Badge>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '3' })}
+                onClick={() => { toggleTab('3'); }}
+              >
+                <Badge href="#" color="warning" pill>Vizualizado</Badge>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '4' })}
+                onClick={() => { toggleTab('4'); }}
+              >
+                <Badge href="#" color="success" pill>Revisado</Badge>
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
+                    <div className="solicitacaoItems">     
+                      <CardTitle><b>Caio Vinícius Santos Lopes</b></CardTitle>
+                      <Button color="warning" onClick={toggle}><FaRegEye/></Button>
+                    </div>
+                    <CardBody className="simpleCard">
+                    </CardBody>
+                  </Card>
+                  <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
+                    <div className="solicitacaoItems">     
+                      <CardTitle className="solicitacaoTitle"><b>Daniele Freire Santos</b></CardTitle>
+                      <Button color="info" onClick={toggle}><FaRegEye/></Button>
+                    </div>
+                    <CardBody className="simpleCard">
+                    </CardBody>
+                  </Card>
+                  <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
+                    <div className="solicitacaoItems">     
+                      <CardTitle className="solicitacaoTitle"><b>Luan Lemos</b></CardTitle>
+                      <Button color="success" onClick={toggle}><FaRegEye/></Button>
+                    </div>
+                    <CardBody className="simpleCard">
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="12">
+                  <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
+                    <div className="solicitacaoItems">     
+                      <CardTitle className="solicitacaoTitle"><b>Daniele Freire Santos</b></CardTitle> 
+                      <Button color="info" onClick={toggle}><FaRegEye/></Button>
+                    </div>
+                    <CardBody className="simpleCard">
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="3">
+              <Row>
+                <Col sm="12">
+                  <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
+                    <div className="solicitacaoItems">     
+                      <CardTitle><b>Caio Vinícius Santos Lopes</b></CardTitle>                
+                      <Button color="warning" onClick={toggle}><FaRegEye/></Button>
+                    </div>
+                    <CardBody className="simpleCard">
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="4">
+              <Row>
+                <Col sm="12">
+                  <Card className="solicitacao" body inverse style={{ backgroundColor: '#030F11', borderColor: '#030F11', height:'70px' }}>
+                    <div className="solicitacaoItems">     
+                      <CardTitle className="solicitacaoTitle"><b>Luan Lemos</b></CardTitle>
+                    <Button color="success" onClick={toggle}><FaRegEye/></Button>
+                    </div>
+                    <CardBody className="simpleCard">
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
         </div>
-        {/* <Form>
-          <FormGroup>
-            <Label for="materias">Editar a grade do aluno</Label>
-            <Input type="select" name="materias" id="materias" multiple>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </Input>
-            <FormText color="muted">
-              Avalie a grade montada pelo aluno.
-            </FormText>
-          </FormGroup>
-          <FormGroup>
-            <Label for="comentario">Comentário</Label>
-            <Input type="textarea" name="comentario" id="comentario" />
-          </FormGroup>
-          <Button>Submit</Button>
-        </Form> */}
+        <div>
+        <div>
+          <Modal isOpen={modal} toggle={toggle} className={className}>
+            <ModalHeader toggle={toggle}>{name}</ModalHeader>
+            <ModalBody>
+              <p>Matrícula: {matricula}</p>
+              <p>Semestre de Ingresso: {semestre}</p>
+              <p>CR:{cr}</p>
+              <p>Estágia ou trabalha?: {trabalho}</p>
+              <p>Participa de alguma IC ou extensão?: {extensao}</p>
+              <Form>
+                <FormGroup>
+                  <Label for="materias">Editar a grade do aluno</Label>
+                  <Input type="select" name="materias" id="materias" multiple>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Input>
+                  <FormText color="muted">
+                    Avalie a grade montada pelo aluno.
+                  </FormText>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="comentario">Comentário</Label>
+                  <Input type="textarea" name="comentario" id="comentario" />
+                </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="info" onClick={toggle}>Confirmar</Button>{' '}
+              <Button color="secondary" onClick={toggle}>Cancelar</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+        </div>
       </div>
-)
+);
+
+}
 
 export default Professor;
